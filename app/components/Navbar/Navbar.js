d452c76d1f0691e44b85ae7874803de5e0258780
Cristianodhoni09 <amitkum9898@gmail.com>
2024-12-29 17:13:47 +0530

    Add scroll effect to navbar with styling adjustments

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -1,9 +1,29 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import styles from './navbar.module.css';
-import Link from 'next/link';
 
 export default function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className={styles.navbar}>
+    <nav className={`${styles.navbar} ${scrolled ? styles.scrolled : ''}`}>
       <div className={styles.brand}>
         &lt;Amit Kumar /&gt;
       </div>
